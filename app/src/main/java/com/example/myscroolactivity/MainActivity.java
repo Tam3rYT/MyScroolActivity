07da09c7616811eb6da088aa1f09da9da5192cf0
Hamam Tamer <tamer.hamam@issm.network>
2021-04-27 09:54:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myscroolactivity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myscroolactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
